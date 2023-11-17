7d1dc48d20dd028940bd63537b9a615f8d961758
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-11-17 11:56:11 +0500

    Nice to meet you

---
+++ MyFirstCmakeProj/MyFirstCmakeProj.h
@@ -0,0 +1,12 @@
+﻿// MyFirstCmakeProj.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
